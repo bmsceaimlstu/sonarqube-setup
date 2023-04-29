f5474be6e6b7f0462be2d42115d58c52db24c01f
bmsceaimlstu <89834651+bmsceaimlstu@users.noreply.github.com>
2023-04-29 16:48:50 +0530

    adding nonsense code

--- app/controllers/api/v1/test.js
+++ app/controllers/api/v1/test.js
@@ -10,3 +10,20 @@
 
 // module.exports = router;
 
+
+<script>
+	let area = function (length, breadth) {
+		let a = length * breadth;
+		console.log('Area of the rectangle is ' + a + ' square unit');
+	}
+	
+	let perimeter = function (length, breadth) {
+		let p = 2 * (length + breadth);
+		console.log('Perimeter of the rectangle is ' + p + ' unit');
+	}
+	
+	module.exports = {
+		area,
+		perimeter
+	}
+</script>
