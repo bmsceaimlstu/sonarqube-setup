8c497bb90491aaf9c4734d149b81f0023f23ee3e
bmsceaimlstu <89834651+bmsceaimlstu@users.noreply.github.com>
2023-04-29 16:45:02 +0530

    bulk changes 2

--- app/controllers/api/v1/index.js
+++ app/controllers/api/v1/index.js
@@ -4,3 +4,5 @@ const router = require('express').Router(),
 router.use('/test', testController);
 
 module.exports = router;
+
+hello there I want this to not be recognised
--- app/controllers/api/v1/test.js
+++ app/controllers/api/v1/test.js
@@ -1,12 +1,12 @@
-const express = require('express'),
-  router = express.Router();
+// const express = require('express'),
+//   router = express.Router();
 
-router.get('/', (req, res) => {
-  const result = {};
-  result.empId = 'EMP-123';
-  result.empName = 'John';
-  res.status(200).json({ success: true, data: result });
-});
+// router.get('/', (req, res) => {
+//   const result = {};
+//   result.empId = 'EMP-123';
+//   result.empName = 'John';
+//   res.status(200).json({ success: true, data: result });
+// });
 
-module.exports = router;
+// module.exports = router;
 
