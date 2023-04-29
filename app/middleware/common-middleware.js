2a22301973bccd4ff758ea571963bc8d939b979d
bmsceaimlstu <89834651+bmsceaimlstu@users.noreply.github.com>
2023-04-29 16:40:18 +0530

    major changes

--- app/middleware/common-middleware.js
+++ app/middleware/common-middleware.js
@@ -1,8 +1,8 @@
-const middlewares = {
-  setDefaultHeaders(req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    next();
-  },
-};
+// const middlewares = {
+//   setDefaultHeaders(req, res, next) {
+//     res.setHeader('Content-Type', 'application/json');
+//     next();
+//   },
+// };
 
-module.exports = middlewares;
+// module.exports = middlewares;
